92d58df99e98281b499f38b6337a2b06125e15ba
Tyra Levin <tyra@rackham3.uppmax.uu.se>
2023-04-17 10:34:27 +0200

    added code files for RNA trimming with Trimmomatic

---
+++ code/trim_RNA_SRR4342139.sh
@@ -0,0 +1,2 @@
+module load bioinfo-tools trimmomatic/0.39
+java -jar $TRIMMOMATIC_ROOT/trimmomatic.jar PE /home/tyra/GA_2023/data/raw_data/RNA_untrimmed_provided/SRR4342139.1.fastq.gz /home/tyra/GA_2023/data/raw_data/RNA_untrimmed_provided/SRR4342139.2.fastq.gz SRR4342139_RNA_1_paired.fq.gz SRR4342139_RNA_1_unpaired.fq.gz SRR4342139_RNA_2_paired.fq.gz SRR4342139_RNA_2_unpaired.fq.gz ILLUMINACLIP:illumina_adap_2.fa:2:30:10 LEADING:12 TRAILING:6 MINLEN:50
