a1a4ed2062f23302e61dbb3adc00a1640e85bc60
Tyra Levin <tyra@s65.uppmax.uu.se>
2023-05-17 14:55:55 +0200

    batch scripts for DNA alignment of the DNA data from both locations to the assembly using BWA mem

---
+++ code/DNA_2133_BWA_depth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 08:00:00
+#SBATCH -J BWA_DNA_2133
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user tyra.levin.3011@student.uu.se
+# Load modules
+module load bioinfo-tools
+module load bwa/0.7.17
+module load samtools/1.17
+# Your commands
+bwa mem asm2 /home/tyra/GA_2023/data/trimmed_data/DNA_trimmed_provided/SRR4342133_1.paired.trimmed.fastq.gz /home/tyra/GA_2023/data/trimmed_data/DNA_trimmed_provided/SRR4342133_2.paired.trimmed.fastq.gz -t 4 | samtools view -u -b - > /home/tyra/GA_2023/analyses/03_binning/bwa_aln/BWA_DNA_2133.bam
+
