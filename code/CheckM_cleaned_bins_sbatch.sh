def78bc2966d003f0cc2dc41ed55786e4752acad
Tyra Levin <tyra@s65.uppmax.uu.se>
2023-05-17 15:01:22 +0200

    batch script for quality assessment of outlier cleaned bins using CheckM

---
+++ code/CheckM_cleaned_bins_sbatch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 04:00:00
+#SBATCH -J Tyra_checkM_clean_checkm
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user tyra.levin.3011@student.uu.se
+# Load modules
+module load bioinfo-tools
+module load CheckM/1.1.3
+# Your commands
+checkm lineage_wf --reduced_tree -t 4 -x fa /home/tyra/GA_2023/analyses/04_quality_assessment_binning/outlier_cleaned_bins /home/tyra/GA_2023/analyses/04_quality_assessment_binning/CheckM_results_cleaned_bins
+
